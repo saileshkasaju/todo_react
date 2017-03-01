5b9a1814dee2d7d279447c7c4549de065b87d73e
saileshkasaju <leosailesh@gmail.com>
2017-03-01 08:05:35 +0545

    show temp message

--- src/lib/TodoHelpers.js
+++ src/lib/TodoHelpers.js
@@ -1,40 +1,40 @@
-export const addTodo = (list, item) => Object.assign([], [...list,item])
+export const addTodo = (list, item) => Object.assign([], [...list,item]);
 // alternatively list.concat(item)
 // or [...list, item]
 
-export const generateId = () => Math.floor(Math.random() * 100000)
+export const generateId = () => Math.floor(Math.random() * 100000);
 
-export const findById = (id, list) => list.find(item => item.id === id)
+export const findById = (id, list) => list.find(item => item.id === id);
 
-export const toggleTodo = (todo) => Object.assign({},{...todo, isCompleted: !todo.isCompleted})
+export const toggleTodo = (todo) => Object.assign({},{...todo, isCompleted: !todo.isCompleted});
 // alternatively {...todo, isCompleted: !todo.isCompleted}
 
 export const updateTodo = (list, updated) => {
-	const updatedIndex = list.findIndex(item => item.id === updated.id)
+	const updatedIndex = list.findIndex(item => item.id === updated.id);
 
 	return Object.assign([], [
 		...list.slice(0, updatedIndex),
 		updated,
 		...list.slice(updatedIndex + 1)
-	])
+	]);
 /* alternatively [
 	...list.slice(0, updatedIndex),
 	updated,
 	...list.slice(updatedIndex + 1)
 ] */
-}
+};
 export const removeTodo = (list, id) => {
 	const removeIndex = list.findIndex(item => item.id === id)
 
 	return Object.assign([], [
 		...list.slice(0, removeIndex),
 		...list.slice(removeIndex + 1)
-	])
+	]);
 	/* alternatively [
 	...list.slice(0, removeIndex),
 	...list.slice(removeIndex + 1)
 ] */
-}
+};
 export const filterTodos = (list, route) => {
   switch(route) {
     case '/active':
@@ -44,4 +44,4 @@ export const filterTodos = (list, route) => {
     default:
       return list
   }
-}
+};
